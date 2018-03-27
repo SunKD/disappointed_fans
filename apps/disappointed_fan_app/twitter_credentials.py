7b413646b8b56df32515838df2218dd17138a55a
Chanthy Ngin <nginchanthy@gmail.com>
2018-03-27 16:03:19 -0700

    added twitter api module and remove *.pyc

---
+++ apps/disappointed_fan_app/twitter_credentials.py
@@ -0,0 +1,24 @@
+# == OAuth Authentication ==
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+
+# key1
+# consumer_key = "4uas7JloSuW1RI9mIhWxJpXEl"
+# consumer_secret = "jOwtevpst5HWsIDmxCBiK086Cy7V1ZXDx26yDKHIGpwKg1TcfR"
+
+# # The access tokens can be found on your applications's Details
+# # page located at https://dev.twitter.com/apps (located
+# # under "Your access token")
+# access_token = "978374762350067712-WhFLN9p5MXxZ7GDWJ1YtmkoGQMJNiiz"
+# access_token_secret = "h7RS3jkGSl18BAqPkuIhgxG1mhtdTJ3rKbvm6PI3sIBnU"
+
+# key2
+consumer_key = "qT6nlV564FeQc4Ue9sB0aRwDg"
+consumer_secret = "FGlR7Hw5zaedcRuyFkplbK8rbvCKCbbrGIOg91Stxi35n7Hahv"
+
+access_token = "853729586-lXiNXf1nB6CFX5mcwwB03kaYGU3q2Yll6ypbXjQZ"
+access_token_secret = "x9QSlhuoH3O7CcxOxL8pabVgoEVxpODVCKVskbmNO0x2d"
--- apps/disappointed_fan_app/views.py
+++ apps/disappointed_fan_app/views.py
@@ -2,9 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+import tweepy
+import twitter_credentials
+import datetime
 
 # Create your views here.
 def index(request):
+    twitter_hello()
     return render(request, 'disappointed_fan_app/index.html')
 
 def process(request):
@@ -12,3 +16,74 @@ def process(request):
         "input": request.POST['search']
     }
     return render(request, 'disappointed_fan_app/result.html', context)
+
+def twitter_hello():
+
+    auth = tweepy.OAuthHandler(twitter_credentials.consumer_key, twitter_credentials.consumer_secret)
+    auth.set_access_token(twitter_credentials.access_token, twitter_credentials.access_token_secret)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    # api = tweepy.API(auth)
+
+    # public_tweets = api.home_timeline()
+    # for tweet in public_tweets:
+    #     print tweet.text
+
+    # api = tweepy.API(auth, wait_on_rate_limit=True)
+    #####United Airlines
+    # Open/Create a file to append data
+    # csvFile = open('ua.csv', 'a')
+    #Use csv Writer
+    # csvWriter = csv.writer(csvFile)
+
+    data = {}
+    counter = 0
+
+    today = datetime.date.day
+    print today
+
+    d1 = datetime.date(2000, 2, 1) - datetime.timedelta (days = 1)
+    print d1
+    d1 = datetime.datetime.now() - datetime.timedelta (days = 1)
+    print d1
+    
+    # # datetime.date(2000, 1, 31)
+    for tweet in tweepy.Cursor(api.search, q="#football",
+                            lang="en").items():
+        # print (tweet.created_at, tweet.text)
+        dt = tweet.created_at
+        # print tweet.created_at, dt.month, dt.year, dt.day
+        year_month = "{:04d}-{:02d}-{:02d}".format(dt.year, dt.month, dt.day)
+        # print year_month
+        counter += 1
+        if year_month not in data:
+            counter = 0
+        data[year_month] = counter
+        if len(data) >= 7:
+            break
+
+    list_key = []
+    for i in range(7, 0, -1):
+        dt = datetime.datetime.now() - datetime.timedelta (days = i)
+        print dt
+        year_month = "{:04d}-{:02d}-{:02d}".format(dt.year, dt.month, dt.day)
+        list_key.append(year_month)
+        if year_month not in data:
+            data[year_month] = 0
+
+    # # for tweet in tweepy.Cursor(api.search, q="#wowww",
+    # #                         lang="en").items():
+    # #     # print (tweet.created_at, tweet.text)
+    # #     dt = tweet.created_at
+    # #     # print tweet.created_at, dt.month, dt.year, dt.day
+    # #     year_month = "{:04d}-{:02d}-{:02d}".format(dt.year, dt.month, dt.day)
+    # #     # print year_month
+    # #     counter += 1
+    # #     if year_month not in data:
+    # #         counter = 0
+    # #     data[year_month] = counter
+
+    print data
+    print len(data)
+    print list_key
+        # csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
