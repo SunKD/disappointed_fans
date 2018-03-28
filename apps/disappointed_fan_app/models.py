e89b1d4ef82cf57c80fcecc1eac67314f10e09a4
petersonjustin <peterson.justin11@gmail.com>
2018-03-28 10:10:48 -0700

    initial setup of Search table and starting validations

--- apps/disappointed_fan_app/models.py
+++ apps/disappointed_fan_app/models.py
@@ -3,4 +3,34 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# importing regex for validations
+import re
+
+
+class WordManager(models.Manager):
+    # manager for user inputted words
+
+    def user_input_validator(self):
+        # used to validate the words that users enter
+        pass
+
+    def reject_word(self):
+        # used to check if word is already in DB as rejected word based on the "allowed" stored data attribute. Boolean.
+        pass
+
+
+
+
+
+
+
+
+
+class User_Search(models.Model):
+    # used to store the
+    search_data = models.CharField(max_length=100)
+    allowed = models.SmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __repr__(self):
+        return "Word Desc = word: {} - created_at: {}".format(self.word, self.created_at)
